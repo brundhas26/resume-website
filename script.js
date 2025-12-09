c0f7bb01dc496f1fa18e5ba6eb6c062215fd54a3
brundhas26 <brundas265@gmail.com>
2025-12-09 12:18:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple interactivity for button
+document.getElementById('downloadResume')?.addEventListener('click', () => {
+  // For now it opens resume page
+  window.open('resume.html', '_blank');
+});
